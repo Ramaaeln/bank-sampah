ec970e41fbbbcb52ede42cc336a205f47d8e47c5
Ramaaeln <abdullahramadanelansary@gmail.com>
2026-01-07 10:51:19 +0700

    first commit

--- be/src/app.js
+++ be/src/app.js
@@ -5,10 +5,14 @@ import userRoutes from './routes/user.routes.js';
 const app = express();
 
 app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: ['https://bank-sampah-rust.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false
 }));
+
+// 🔥 INI YANG KURANG
+app.options('*', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
